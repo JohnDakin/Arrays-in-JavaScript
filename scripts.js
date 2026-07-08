72d961e6468949ef8ebce77a76590b38e8014090
JohnDakin <jdakin749@gmail.com>
2026-07-08 22:05:00 +0300

    Added script.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let names = ["Danny", "Joe", "Sarah", "Molly"];
+let names2 = ["John", "Tom"];
+
+// let stringNames = names.toString();
+// let stringNames = names.join(' and ');
+// console.log(stringNames);
+
+//concat
+let joined = names.concat(names2);
+console.log(joined);
+
